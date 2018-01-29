5c13b40e8433f10ed100c1067248316be64e35e6
S.j. Sakib <sjsakib.bd@gmail.com>
2018-01-29 20:12:53 +0600

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEYS

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@a0d3cw$@rtg1nv4cv=gk5rc6g^h&9dcvya1p%5!otu^%vu3p('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEGUB_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = '@a0d3cw$@rtg1nv4cv=gk5rc6g^h&9dcvya1p%5!otu^%vu3p('
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
